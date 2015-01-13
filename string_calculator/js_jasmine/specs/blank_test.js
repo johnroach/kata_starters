21445cefc20d84244056729e8c36a5deb08cf61d
Randy Eppinger <randy.eppinger@leandog.com>
2015-01-13 09:18:24 -0500

    Adding basic file structure.

---
+++ string_calculator/js_jasmine/specs/blank_test.js
@@ -0,0 +1,8 @@
+describe('javascript library or filename', function (){
+    describe('function name or sub-library name', function() {
+       it('should have some behavior', function(){
+          expect(true).toBe(false);
+       });
+    });
+
+});
