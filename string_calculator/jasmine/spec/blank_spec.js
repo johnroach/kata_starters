c80f6e09b09a52ad1dcf10314b9d7eae600e5176
Randy Eppinger <randy.eppinger@leandog.com>
2016-06-14 15:54:10 -0400

    Added karma configuration which seems to allow me to us ES2016 syntax.

--- string_calculator/jasmine/spec/blank_spec.js
+++
@@ -1,8 +0,0 @@
-describe('javascript library or filename', function (){
-    describe('function name or sub-library name', function() {
-       it('should have some behavior', function(){
-          expect(true).toBe(false);
-       });
-    });
-
-});
---
+++ string_calculator/jasmine/spec/string_calculator_spec.js
@@ -0,0 +1,9 @@
+describe('String Calculator', function (){
+    describe('add', function() {
+       it('should behave thusly with given input', function(){
+       	  
+          expect(1).toBe(0);
+       });
+    });
+
+});
