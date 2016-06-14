b4472a40c37ff5ac7c34e91ddff1359e0abdd64e
Randy Eppinger <randy.eppinger@leandog.com>
2016-06-14 16:32:28 -0400

    Slightly adjusted starting point of sample spec.

--- string_calculator/jasmine/spec/string_calculator_spec.js
+++ string_calculator/jasmine/spec/string_calculator_spec.js
@@ -1,6 +1,7 @@
 describe('String Calculator', function (){
     describe('add', function() {
-       it('should behave thusly with given input', function(){
+       it('behaves thusly with given input', function(){
+       	  let calculator = new StringCalculator();
        	  
           expect(1).toBe(0);
        });
