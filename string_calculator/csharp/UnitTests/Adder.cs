be8931d729f0d171df3b79ed2fe39be6d020b923
rxe870 <randy.eppinger@leandog.com>
2016-06-15 12:20:47 -0400

    Added C# version of StringCalculator.

---
+++ string_calculator/csharp/UnitTests/Adder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UnitTests
+{
+    internal class Adder
+    {
+  
+        internal int Add(string numbers)
+        {
+            var result = 0;
+            int.TryParse(numbers, out result);
+            return result;
+        }
+    }
+}
