7613edf77663b7c66ddd50e69b3dd5018ffc04a7
Randy Eppinger <randy.eppinger@leandog.com>
2016-06-15 10:39:34 -0400

    Finished setup of Jasmine FizzBuzz.

---
+++ fizzbuzz/jasmine/spec/fizzbuzz_spec.js
@@ -0,0 +1,10 @@
+describe('FizzBuzz', function (){
+    describe('print', function() {
+       it('behaves thusly', function(){
+       	  let fizzbuzzer = new FizzBuzz();
+       	  
+          expect(1).toBe(0);
+       });
+    });
+
+});
