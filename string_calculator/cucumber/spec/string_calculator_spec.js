c3714689841311f4e6950fe255202943cdc78185
Randy Eppinger <randy.eppinger@leandog.com>
2016-09-30 12:41:01 -0400

    Made cucumber version stand-alone adding jasmine stuff to do true ATDD in kata.

---
+++ string_calculator/cucumber/spec/string_calculator_spec.js
@@ -0,0 +1,10 @@
+describe('String Calculator', function (){
+    describe('add', function() {
+       it('behaves thusly with given input', function(){
+       	  let calculator = new StringCalculator();
+
+          expect(1).toBe(0);
+       });
+    });
+
+});
