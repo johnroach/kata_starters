1e2e0e6066d313f6fdfe6f1dac131912540b07a9
Randy Eppinger <randy.eppinger@leandog.com>
2016-10-04 14:01:45 -0400

    Implemented step to click calculate button.

--- string_calculator/cucumber/features/step_definitions/my_steps.rb
+++ string_calculator/cucumber/features/step_definitions/my_steps.rb
@@ -1,3 +1,7 @@
 Given(/^I navigate to the string calculator page$/) do
   visit(StringCalculatorPage)
 end
+
+When(/^I calculate the sum with no input$/) do
+  on(StringCalculatorPage).calculate
+end
