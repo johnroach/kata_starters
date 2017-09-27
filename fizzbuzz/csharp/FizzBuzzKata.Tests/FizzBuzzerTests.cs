cc3bac6bbd1c3bd312df24f58d735482e34d3fca
Randy Eppinger <randy@eppingerconsulting.com>
2017-09-27 13:53:00 -0400

    Added C# .NET Standard version of FizzBuzz starter.

---
+++ fizzbuzz/csharp/FizzBuzzKata.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using FizzBuzzKata;
+
+namespace KataStarter.Tests
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        [Test]
+        public void ATest()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+            Assert.Fail();
+        }
+    }
+}
---
+++ fizzbuzz/csharp/FizzBuzzKata.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KataStarter.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KataStarter")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f33f5bc-0637-43fc-b23d-0b998176e74c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
